5b851f5d5ffdd5c34f47f955c2aaa41f4b372b5a
Anuj-010 <anujpatwari5555@gmail.com>
2024-10-07 13:08:41 +0530

    completed

---
+++ Data/index.ts
@@ -0,0 +1,216 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  // { name: "Testimonials", link: "#testimonials" },
+  { name: "Work Experience", link: "#experience"},
+  { name: "Contact", link: "#contact" },
+];
+
+export const GridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently Working on my upcoming projects",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Fetch AI",
+    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    img: "/temp-banner.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/icons8-flow-24.png", "/icons8-postgres.svg"],
+    link: "https://github.com/priyanshu-7938/fetch_comp_agetnt_orchestration",
+  },
+  
+  {
+    id: 2,
+    title: "TraMck",
+    des: "Track your expenses with this cool tool TraMck.",
+    img: "/6.png",
+    iconLists: ["/icons8-html.svg", "/tail.svg", "icons8-js.svg", "icons8-php.svg", "icons8-mysql.svg"],
+    link: "https://github.com/Anuj-010/TraMck",
+  },
+  // {
+  //   id: 3,
+  //   title: "To-Do list app",
+  //   des: "First project where i learn the basics of web and the most fundamental language of the web.",
+  //   img: "/todo.png",
+  //   iconLists: ["/icons8-html.svg", "/icons8-css.svg", "icons8-js.svg",],
+  //   link: "/ui.earth.com",
+  // },
+ 
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Backend Developer",
+    desc: "Got an incredible chance to contribute to a project which helps our college to schedule our busy seminar hall.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail:  "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Head Co-ordinator of Alpha Coding Club",
+    desc: "I was one of the head cordinator of the coding club of our college, where I lead a team of 20 other students and organise varios events in our college.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance video editor",
+    desc: "Got my first gig as freelancer in the domain of video editing for shooting an ad of a jwellery shop.",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+
+  // {
+  //   id: 4,
+  //   title: "Backend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    link:"https://github.com/Anuj-010",
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    link:"https://x.com/Anuj01010",
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    link:"https://www.linkedin.com/in/anuj-patwari-512923238/",
+    img: "/link.svg",
+  },
+];
